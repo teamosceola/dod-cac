e78b96d01e5bfa288a205afa1cfd4ea5264692d5
teamosceola <christopher.crews07@gmail.com>
2021-10-12 23:36:29 -0500

    fix for firefox-esr

--- setup.sh
+++ setup.sh
@@ -45,8 +45,16 @@ fi
 firefox --headless &
 sleep 10
 killall $(ps a | grep 'firefox --headless' | grep -v grep | sed -r 's/^.* (\/.*) .*$/\1/g')
+killall $(ps a | grep 'firefox-esr --headless' | grep -v grep | sed -r 's/^.* (\/.*) .*$/\1/g')
 echo 'add cac module to firefox'
-[ -d $HOME/.mozilla/firefox ] && modutil -force -dbdir $HOME/.mozilla/firefox/*.default-release -add 'CAC Module' -libfile /usr/lib/x86_64-linux-gnu/pkcs11/opensc-pkcs11.so
+if [ -d $HOME/.mozilla/firefox ]
+then
+    for dir in $(ls -d $HOME/.mozilla/firefox/*.default*)
+    do
+        modutil -force -create -dbdir $dir
+        modutil -force -dbdir $dir -add 'CAC Module' -libfile /usr/lib/x86_64-linux-gnu/pkcs11/opensc-pkcs11.so
+    done
+fi
 if [ -d $HOME/snap/firefox ]
 then
     for dir in $(ls -d $HOME/snap/firefox/common/.mozilla/firefox/*.default*)
@@ -98,6 +106,13 @@ do
     # Import into Firefox browser cert store
     echo "add $cert_name to firefox"
     [ -d $HOME/.mozilla/firefox ] && certutil -A -n $cert_name -t "CT,C,C" -d $HOME/.mozilla/firefox/*.default-release -i $cert
+    if [ -d $HOME/.mozilla/firefox ]
+    then
+        for dir in $(ls -d $HOME/.mozilla/firefox/*.default*)
+        do
+            certutil -A -n $cert_name -t "CT,C,C" -d $dir -i $cert
+        done
+    fi
     if [ -d $HOME/snap/firefox ]
     then
         for dir in $(ls -d $HOME/snap/firefox/common/.mozilla/firefox/*.default*)
