601d69080e40404b974c2e752adc4c9ffb44d74a
TeamOSEOLA <teamosceola@users.noreply.github.com>
2022-12-19 17:34:45 -0600

    Added importing Microsofts GPG key

--- fedora.sh
+++ fedora.sh
@@ -10,5 +10,8 @@
 sudo dnf update --refresh -y
 sudo dnf install dnf-plugins-core unzip nss-tools openssl curl -y
 sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge -y
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc > ./microsoft.asc
+sudo rpm --import ./microsoft.asc
+rm ./microsoft.asc
 sudo dnf update --refresh -y
 sudo dnf install microsoft-edge-stable -y
