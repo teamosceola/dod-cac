4931d795ffb852c9862df7463bc5b36abf589e61
TeamOSEOLA <teamosceola@users.noreply.github.com>
2024-04-15 20:18:24 -0500

    added configuring p11-kit-server user service

--- fedora.sh
+++ fedora.sh
@@ -7,6 +7,7 @@
 # *NOTE: Do NOT run script with 'sudo', run as regular user                                          #
 ######################################################################################################
 
+# Install needed tools
 if [ $(which dnf | echo $?) == "0" ]
 then
     sudo dnf update --refresh -y
@@ -17,5 +18,15 @@ then
     rpm-ostree install --apply-live --idempotent --allow-inactive openssl nss-tools unzip curl -y
 fi
 
+# Enable p11-kit server as user service
+systemctl --user enable p11-kit-server.service
+systemctl --user start p11-kit-server.service
+
+# Add Flathub repo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Install Microsoft Edge from Flathub
 flatpak install flathub com.microsoft.Edge -y
+
+# Override pkcs11 provider to make CAC work in Edge flatpak
+flatpak override -u --filesystem=xdg-run/p11-kit/pkcs11 com.microsoft.Edge
